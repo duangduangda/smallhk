e32be70ff78cbbd6e417b2ed30777640aa37b12b
duangduangda <dongyhemail@sina.com>
2016-10-14 11:36:58 +0800

    updating scheduleTask

--- src/main/java/com/smallhk/core/concurrent/schedule/ScheduleTaskRunner.java
+++ src/main/java/com/smallhk/core/concurrent/schedule/ScheduleTaskRunner.java
@@ -8,34 +8,29 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * start a thread to run schedule task
+ * 
  * @author duangduangda
  *
  */
 public class ScheduleTaskRunner {
 
 	public static void main(String[] args) {
-		Runnable scheduleTask = new Runnable(){
+		Runnable scheduleTask = new Runnable() {
 
 			public void run() {
-				System.out.println(new Date() + "Using multiple thread to run schedule tasks...");
+				System.out.println("[" + Thread.currentThread().getName() + " " + new Date()  + "] Using multiple thread to run schedule tasks...");
 			}
-			
+
 		};
-		
+
 		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-		service.scheduleAtFixedRate(scheduleTask, getInitialDelayTime(), 60*60*1000, TimeUnit.MILLISECONDS);
+		service.scheduleAtFixedRate(scheduleTask, getInitialDelayTime(), 60 * 1000, TimeUnit.MILLISECONDS);
 	}
 
 	private static long getInitialDelayTime() {
-		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.HOUR_OF_DAY, 14);
-		cal.set(Calendar.MINUTE, 5);
-		cal.set(Calendar.SECOND, 0);
-		cal.set(Calendar.MILLISECOND, 001);
-		long initialDelayTime = cal.getTimeInMillis()- new Date().getTime();
-		System.out.println(initialDelayTime);
-		return initialDelayTime;
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 11,
+				33, 00);
+		return calendar.getTimeInMillis() - new Date().getTime();
 	}
 }
-
-
