ffdaad65286caab596a6fcae14b2d526d30d0aae
duangduangda <dongyhemail@gmail.com>
2018-06-28 20:08:40 +0800

    [M]protocol http

---
+++ src/main/java/com/smallhk/netty/in/action/protocol/http/HttpFileServer.java
@@ -0,0 +1,58 @@
+package com.smallhk.netty.in.action.protocol.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * Title. <br>
+ * Description. http文件服务器
+ * <p>
+ * Copyright: Copyright (c) 2018/6/28
+ * <p>
+ * Company:
+ * <p>
+ *
+ * @author: eric
+ * <p>
+ * Version: 1.0
+ * <p>
+ */
+public class HttpFileServer {
+
+    private static final String DEFAULT_URL = "/src/main/java/com/smallhk/netty/in/action";
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler()).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new HttpRequestDecoder());
+                    socketChannel.pipeline().addLast(new HttpObjectAggregator(65536));
+                    socketChannel.pipeline().addLast(new HttpResponseEncoder());
+                    socketChannel.pipeline().addLast(new ChunkedWriteHandler());
+                    socketChannel.pipeline().addLast(new HttpFileServerHandler(DEFAULT_URL));
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
+            System.out.println("HTTP文件目录服务器启动，网址是 : " + "http://localhost:" + 8080 + DEFAULT_URL);
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
