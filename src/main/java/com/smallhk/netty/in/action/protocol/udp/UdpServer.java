e5162f544870a0efc2864f6582b4e26795ad5e72
duangduangda <dongyhemail@gmail.com>
2018-06-29 11:10:49 +0800

    [M]netty protocol:udp

--- src/main/java/com/smallhk/netty/in/action/protocol/udp/UdpServer.java
+++ src/main/java/com/smallhk/netty/in/action/protocol/udp/UdpServer.java
@@ -1,9 +1,6 @@
 package com.smallhk.netty.in.action.protocol.udp;
 
-import com.sun.deploy.association.utility.AppConstants;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
