03b61b05fcef64f89baf568e20475fd9a68a379b
duangduangda <dongyhemail@gmail.com>
2018-10-10 19:22:03 +0800

    [M] rmi

---
+++ src/main/java/com/smallhk/core/rmi/HelloService.java
@@ -0,0 +1,23 @@
+package com.smallhk.core.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Title. <br>
+ * Description.
+ * <p>
+ * Copyright: Copyright (c) 2018/10/10
+ * <p>
+ * Company:
+ * <p>
+ *
+ * @author: eric
+ * <p>
+ * Version: 1.0
+ * <p>
+ */
+public interface HelloService extends Remote{
+
+    String sayHello(String name) throws RemoteException;
+}
---
+++ src/main/java/com/smallhk/core/rmi/HelloServiceImpl.java
@@ -0,0 +1,30 @@
+package com.smallhk.core.rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Title. <br>
+ * Description.
+ * <p>
+ * Copyright: Copyright (c) 2018/10/10
+ * <p>
+ * Company:
+ * <p>
+ *
+ * @author: eric
+ * <p>
+ * Version: 1.0
+ * <p>
+ */
+public class HelloServiceImpl extends UnicastRemoteObject implements HelloService {
+
+    protected HelloServiceImpl() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public String sayHello(String name) throws RemoteException {
+        return "hello," + name;
+    }
+}
