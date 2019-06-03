8a87ebe75135a46232c49eb0d1616bf9c022ed06
yaohua.dong <yaohua.dong@nio.com>
2019-06-03 19:38:19 +0800

    test mock guava basic

---
+++ src/main/java/com/smallhk/guava/OptionalHandler.java
@@ -0,0 +1,33 @@
+package com.smallhk.guava;
+
+import com.google.common.base.Optional;
+
+/**
+ * @description:
+ * @author: dean
+ * @create: 2019/03/10 18:48
+ */
+public class OptionalHandler {
+    public static void main(String[] args) {
+        // 创建指定引用的optional实例，若引用为null则快速失败
+        Optional<Integer> possible = Optional.of(5);
+        System.out.println(possible.isPresent());
+        System.out.println(possible.get());
+        // 创建指定引用的optional实例，如果引用为null,则表示缺失
+        Optional<Integer> nullPossible = Optional.fromNullable(null);
+        System.out.println(nullPossible.isPresent());
+
+        String status = "213";
+        // 如果为空，取123，否则取status的值
+        System.out.println(Optional.fromNullable(status).or("123"));
+        status = null;
+        // 返回optional所包含的引用，如果引用缺失，返回Null
+        System.out.println(Optional.fromNullable(status).orNull());
+
+        // 创建一个引用缺失的optional实例
+        Optional<Integer> absentInteger = Optional.absent();
+        System.out.println(absentInteger.isPresent());
+
+
+    }
+}
---
+++ src/main/java/com/smallhk/guava/OrderingHandler.java
@@ -0,0 +1,31 @@
+package com.smallhk.guava;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
+
+import java.util.List;
+
+/**
+ * @description: 排序器
+ * @author: dean
+ * @create: 2019/06/03 17:38
+ */
+public class OrderingHandler {
+    public static void main(String[] args) {
+        List<Integer> integers = Lists.newArrayList(2, 1, 34, 1, 31, 341);
+        // 创建排序器
+        Ordering<Integer> naturalOrder = Ordering.natural();
+        // 获取可迭代对象中最大的K个元素
+        System.out.println(naturalOrder.greatestOf(integers, 2));
+        // 正排序
+        System.out.println(naturalOrder.sortedCopy(integers));
+        // 逆排序
+        System.out.println(naturalOrder.reverse().sortedCopy(integers));
+        // 是否严格有序(不允许出现重复的值)
+        System.out.println(naturalOrder.isStrictlyOrdered(naturalOrder.sortedCopy(integers)));
+        // 返回最大值，最小值
+        System.out.println(naturalOrder.max(integers));
+        System.out.println(naturalOrder.min(integers));
+
+    }
+}
