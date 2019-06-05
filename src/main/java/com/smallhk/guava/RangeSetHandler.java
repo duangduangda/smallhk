ff9cdfe0f9185a759d0caf30eac7e6c7a881dea8
yaohua.dong <yaohua.dong@nio.com>
2019-06-05 14:34:22 +0800

    Sets,RangeSet

--- src/main/java/com/smallhk/guava/RangeSetHandler.java
+++ src/main/java/com/smallhk/guava/RangeSetHandler.java
@@ -13,8 +13,27 @@ public class RangeSetHandler {
     public static void main(String[] args) {
         RangeSet<Integer> rangeSet = TreeRangeSet.create();
         // 闭区间[1-10]
-        rangeSet.add(Range.open(1, 10));
+        rangeSet.add(Range.closed(1, 10));
         System.out.println(rangeSet);
-        System.out.println();
+        // 添加左开右闭区间,由于和之前的区间发生了重合，重合的部分将会合并
+        rangeSet.add(Range.openClosed(10, 26));
+        System.out.println(rangeSet);
+        // 添加空区间，将会被忽略
+        rangeSet.add(Range.openClosed(0, 0));
+        System.out.println(rangeSet);
+        // 分割操作
+        rangeSet.remove(Range.closed(5, 10));
+        System.out.println(rangeSet);
+        // 返回补集
+        System.out.println(rangeSet.complement());
+        // 遍历RangeSet中的Range
+        System.out.println(rangeSet.asRanges());
+        // 是否包含某个元素
+        System.out.println(rangeSet.contains(6));
+        System.out.println(rangeSet.contains(2));
+        // 返回存在给定元素的区间
+        System.out.println(rangeSet.rangeContaining(2));
+        // 返回最小区间
+        System.out.println(rangeSet.span());
     }
 }
---
+++ src/main/java/com/smallhk/guava/SetsHandler.java
@@ -0,0 +1,25 @@
+package com.smallhk.guava;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+/**
+ * @description: guava集合：Sets
+ * @author: dean
+ * @create: 2019/06/05 11:58
+ */
+public class SetsHandler {
+    public static void main(String[] args) {
+        Set<Integer> set1 = Sets.newHashSet(1, 2, 2, 3, 41, 3444);
+        Set<Integer> set2 = Sets.newHashSet(21, 3, 4122, 2);
+        // 交集
+        System.out.println(Sets.intersection(set1, set2));
+        // 并集
+        System.out.println(Sets.union(set1, set2));
+        // 差集,获取set1中除与set2交集之外的元素
+        System.out.println(Sets.difference(set1, set2));
+        // 差集,获取set2中除与set1交集之外的元素
+        System.out.println(Sets.difference(set2, set1));
+    }
+}
