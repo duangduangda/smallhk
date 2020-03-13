f2e73760dc996bd818a335814afc633fd231a9c5
yaohua.dong <yaohua.dong@nio.com>
2020-03-13 23:10:15 +0800

    change netty pom version

--- src/main/java/org/dean/duck/netty/in/action/examples/simpletcp/SimpleTcpClientHandler.java
+++ src/main/java/org/dean/duck/netty/in/action/examples/simpletcp/SimpleTcpClientHandler.java
@@ -3,15 +3,21 @@ package org.dean.duck.netty.in.action.examples.simpletcp;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * 一般用netty来发送和接收数据都会继承SimpleChannelInboundHandler和ChannelInboundHandlerAdapter这两个抽象类，那么这两个到底有什么区别呢？
+ * <p>
+ * 　　其实用这两个抽象类是有讲究的，在客户端的业务Handler继承的是SimpleChannelInboundHandler，而在服务器端继承的是ChannelInboundHandlerAdapter。
+ * <p>
+ * 　　最主要的区别就是SimpleChannelInboundHandler在接收到数据后会自动release掉数据占用的Bytebuffer资源(自动调用Bytebuffer.release())。而为何服务器端不能用呢，因为我们想让服务器把客户端请求的数据发送回去，而服务器端有可能在channelRead方法返回前还没有写完数据，因此不能让它自动release。
+ *
  * @author eric
  */
-public class SimpleTcpClientHandler extends ChannelInboundHandlerAdapter {
+public class SimpleTcpClientHandler extends SimpleChannelInboundHandler<Object> {
 
 	private final static Logger log = LoggerFactory.getLogger(SimpleTcpClientHandler.class);
 
@@ -22,6 +28,11 @@ public class SimpleTcpClientHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+	}
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
 		ByteBuf byteBuf = (ByteBuf) msg;
 		log.info("from server:" + byteBuf.toString(CharsetUtil.UTF_8));
 		log.info("server address:" + ctx.channel().remoteAddress());
--- src/main/java/org/dean/duck/netty/in/action/examples/simpletcp/SimpleTcpServerHandler.java
+++ src/main/java/org/dean/duck/netty/in/action/examples/simpletcp/SimpleTcpServerHandler.java
@@ -9,6 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * 一般用netty来发送和接收数据都会继承SimpleChannelInboundHandler和ChannelInboundHandlerAdapter这两个抽象类，那么这两个到底有什么区别呢？
+ * <p>
+ * 　　其实用这两个抽象类是有讲究的，在客户端的业务Handler继承的是SimpleChannelInboundHandler，而在服务器端继承的是ChannelInboundHandlerAdapter。
+ * <p>
+ * 　　最主要的区别就是SimpleChannelInboundHandler在接收到数据后会自动release掉数据占用的Bytebuffer资源(自动调用Bytebuffer.release())。而为何服务器端不能用呢，因为我们想让服务器把客户端请求的数据发送回去，而服务器端有可能在channelRead方法返回前还没有写完数据，因此不能让它自动release。
+ *
  * @author eric
  */
 public class SimpleTcpServerHandler extends ChannelInboundHandlerAdapter {
